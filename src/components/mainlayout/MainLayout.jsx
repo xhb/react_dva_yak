7c0ae865602c80fc756dcad5d69ccfb21bb67855
xhb <programstart@163.com>
2016-11-19 11:48:20 +0800

    fixed sidebar

--- src/components/mainlayout/MainLayout.jsx
+++ src/components/mainlayout/MainLayout.jsx
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
 import { Menu, Breadcrumb, Icon } from 'antd';
 import styles from "./mainlayout.css";
 
@@ -8,10 +9,23 @@ class MainLayout extends Component {
     super(props);
     this.state = {
       collapse: true,
+      SubMenuAnaOpened: false
     };
   }
 
+  onSubMenuAnaChange(event){
+    this.setState(...this.state, {SubMenuAnaOpened: !this.state.SubMenuAnaOpened})
+  }
+
   onCollapseChange() {
+
+    if(!this.state.collapse){
+      let subMenuAnalysisText = this.refs.analysisRef;
+      if(this.state.SubMenuAnaOpened){
+        subMenuAnalysisText.click();
+      }
+    }
+
     this.setState({
       collapse: !this.state.collapse,
     })
@@ -19,26 +33,34 @@ class MainLayout extends Component {
 
   render() {
     const SubMenu = Menu.SubMenu;
+    const MenuItemGroup = Menu.ItemGroup;
     const collapse = this.state.collapse;
     return (
       <div className={collapse ? (styles.antLayoutAsideCollapse + " " +  styles.antLayoutAside ) : styles.antLayoutAside} >
         <aside className={styles.antLayoutSider} >
           <div className={styles.antLayoutLogo}>Yak</div>
           <Menu mode="inline" theme="dark" defaultSelectedKeys={['user']} className={styles.antMenu} >
+            <Menu.Divider/>
             <Menu.Item key="user" className={styles.antMenuItem}>
-              <Icon type="user" /><span className={styles.navText}>运行情况</span>
+              <Icon type="pie-chart" /><span className={styles.navText}>运行情况</span>
             </Menu.Item>
             <Menu.Item key="setting" className={styles.antMenuItem}>
-              <Icon type="setting" /><span className={styles.navText}>导航二</span>
+              <Icon type="setting" /><span className={styles.navText}>用户场景</span>
             </Menu.Item>
             <Menu.Item key="laptop" className={styles.antMenuItem}>
-              <Icon type="laptop" /><span className={styles.navText}>导航三</span>
+              <Icon type="laptop" /><span className={styles.navText}>场景执行</span>
             </Menu.Item>
-            <Menu.Item key="notification" className={styles.antMenuItem}>
-              <Icon type="notification" /><span className={styles.navText}>导航四</span>
-            </Menu.Item>
+
+            <SubMenu className={styles.antMenuItem} key="analysis" title={<span><Icon type="area-chart" /><span className={styles.navText} ref="analysisRef" >结果分析</span></span>}  disabled={collapse} onTitleClick={this.onSubMenuAnaChange.bind(this)} >
+              <MenuItemGroup  title="报告类型">
+                <Menu.Item  key="1">时序图表</Menu.Item>
+                <Menu.Item  key="2">表格类型</Menu.Item>
+                <Menu.Item  key="3">JSON类型</Menu.Item>
+              </MenuItemGroup>
+            </SubMenu>
+
             <Menu.Item key="folder" className={styles.antMenuItem}>
-              <Icon type="folder" /><span className={styles.navText}>导航五</span>
+              <Icon type="folder" /><span className={styles.navText}>测试报告</span>
             </Menu.Item>
           </Menu>
           <div className={styles.antAsideAction} onClick={this.onCollapseChange.bind(this)}>
