49ad469cfa29adabf2b706113099220e6667d074
xhb <programstart@163.com>
2017-01-21 09:30:55 +0800

    节前提交

--- src/components/analysis/report/navigator.jsx
+++ src/components/analysis/report/navigator.jsx
@@ -9,3 +9,13 @@
        <Bar dataKey="uv" fill="#82ca9d" />
       </BarChart>
 
+      <BarChart width={600} height={300} data={data}
+            margin={{top: 20, right: 30, left: 20, bottom: 5}}>
+       <XAxis dataKey="name"/>
+       <YAxis/>
+       <CartesianGrid strokeDasharray="3 3"/>
+       <Tooltip/>
+       <Legend />
+       <Bar dataKey="pv" stackId="a" fill="#8884d8" />
+       <Bar dataKey="uv" stackId="a" fill="#82ca9d" />
+      </BarChart>
